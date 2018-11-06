1749e4966463b5224e28f289ae66afcad3c3ec87
ToutyRater <ToutyRater@users.noreply.github.com>
2018-11-06 11:23:18 +0000

    Revert source

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,14 @@ mkdir -p h2y
 cd h2y
 rm -rf ./*
 
-#wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_direct_banad.conf
-
-#cat sr_direct_banad.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
+wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_direct_banad.conf
+cat sr_direct_banad.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
 #cat sr_direct_banad.conf | grep Reject|grep CIDR|awk -F, '{print $2}' > ad_ip
-#rm -rf ./sr_direct_banad.conf
+rm -rf ./sr_direct_banad.conf
 #ls -al
 
-wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/factory/resultant/ad.list
-cat ad.list > ad
+#wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/factory/resultant/ad.list
+#cat ad.list > ad
 
 wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/factory/resultant/gfw.list
 cat gfw.list > gfw
