9ab28789d9ab91b3cf4b08c7c85e8d07d23c4a82
cd.zhou <cd.zhou@rvbust.com>
2019-06-27 12:18:22 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git remote add upstream "https://${geo_token}@github.com/ToutyRater/V2Ray-SiteDAT.git"
+git remote add upstream "https://${geo_token}@github.com/zhouscd/V2Ray-SiteDAT.git"
 
-mkdir -p h2y
-cd h2y
+mkdir -p sites
+cd sites
 rm -rf ./*
 
 wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_direct_banad.conf
 cat sr_direct_banad.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
-#cat sr_direct_banad.conf | grep Reject|grep CIDR|awk -F, '{print $2}' > ad_ip
+cat sr_direct_banad.conf | grep Reject|grep CIDR|awk -F, '{print $2}' >> ad
 rm -rf ./sr_direct_banad.conf
 #ls -al
 
@@ -24,7 +24,7 @@ cd ..
 mkdir -p geofiles
 ls -al
 chmod +x ./v2sitedat
-./v2sitedat -dat ./geofiles/h2y.dat -dir ./h2y
+./v2sitedat -dat ./geofiles/geosite.dat -dir ./sites
 
 
 #git checkout -b master
