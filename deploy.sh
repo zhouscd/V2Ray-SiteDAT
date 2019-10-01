c3b26902546cbfc8039c876281d0aa2bd0aef407
zhouscd <zhouscd@gmail.com>
2019-10-01 17:47:28 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,35 @@
 
 git remote add upstream "https://${geo_token}@github.com/zhouscd/V2Ray-SiteDAT.git"
 
+wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_top500_banlist_ad.conf
+
 mkdir -p sites
 cd sites
 rm -rf ./*
 
-wget https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_top500_banlist_ad.conf
-cat sr_top500_banlist_ad.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > addomain
-cat sr_top500_banlist_ad.conf | grep Reject|grep IP-CIDR|awk -F, '{print $2}' > adip
+cat ../sr_top500_banlist_ad.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
+cat ../sr_top500_banlist_ad.conf | grep Proxy |grep DOMAIN-SUFFIX|awk -F, '{print $2}' > gfw
 
-cat sr_top500_banlist_ad.conf | grep Proxy |grep DOMAIN-SUFFIX|awk -F, '{print $2}' > gfwdomain
-cat sr_top500_banlist_ad.conf | grep Proxy |grep IP-CIDR|awk -F, '{print $2}' > gfwip
+cd ..
+
+mkdir -p ips
+cd ips
+rm -rf ./*
+
+cat ../sr_top500_banlist_ad.conf | grep Reject|grep IP-CIDR|awk -F, '{print $2}' > ad
+cat ../sr_top500_banlist_ad.conf | grep Proxy |grep IP-CIDR|awk -F, '{print $2}' > gfw
+cp ../private .
+
+cd ..
+
 rm -rf sr_top500_banlist_ad.conf
 
 
-
-cd ..
-cp private ./sites/
 mkdir -p geofiles
 chmod +x ./v2sitedat
+chmod +x ./v2ipdat
 ./v2sitedat -dat ./geofiles/geosite.dat -dir ./sites
-
+./v2ipdat -dat ./geofiles/geoip.dat -dir ./ips
 
 #git checkout -b master
 git add -A
